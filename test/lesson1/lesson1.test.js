2d4887b9072e994ef05bf60a12f9248abaa26586
linzero <2574025870@qq.com>
2020-02-18 09:50:57 +0800

    add

--- test/lesson1/lesson1.test.js
+++ test/lesson1/lesson1.test.js
@@ -1,4 +1,4 @@
-import revertStrWord from '../../code/lesson/lesson1.js';
+import revertStrWord from '../../code/lesson1/lesson1.js';
 
 test(`revertStrWord:Let's take LeetCode contest`, () => {
     expect(revertStrWord("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
