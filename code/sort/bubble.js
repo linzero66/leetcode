a5f4fd287188210119b0d6d0a72296d098430933
linzhengguang <806307567@qq.com>
2020-03-19 09:37:02 +0800

    add

--- code/sort/bubble.js
+++ code/sort/bubble.js
@@ -24,20 +24,18 @@ let bubbleOpt = (arr) => {
     let k = len - 1
     for (let j = 0; j < len - 1; j++) {
         let flag = 0; //优化外层循环  设置标志位
-
         for (let i = 0; i < k; i++) {
             if (arr[i] > arr[i + 1]) {
                 [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]] //每次都做存储的位置交换，性能较低
-                flag = 1
-                lastExchange = i
+                flag = 1;
+                lastExchange = i;
             }
         }
-        k = lastExchange
+        k = lastExchange;
         //判断标志位是否为0，如果为0，说明后面的元素已经有序，就直接return --1 优化外层循环
         if (flag == 0) {
             break
         }
-
     }
     //排序好的
     return arr
@@ -50,7 +48,7 @@ let bubbleOpt = (arr) => {
 // 说明后面的数比这个数都小，且已经排好序）。下一趟排序开始时，R[1..lastExchange - 1]是无序区，
 // R[lastExchange..n]是有序区。这样，一趟排序可能使当前无序区扩充多个记录，
 // 因此记住最后一次交换发生的位置lastExchange，从而减少排序的趟数
-
+//   优化的冒泡排序只是对一定程度上的有序的序列有提高的时间的作用
 
 //鸡尾酒排序
 let cocktailSort = (arr) => {
